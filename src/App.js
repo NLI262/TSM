69988cdaea51a2cd8bae3447a3459c4ed34df012
NLI262 <53363310+NLI262@users.noreply.github.com>
2019-08-15 20:00:03 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React, {Component} from 'react';
 import './App.css';
+import {BrowserRouter, Route,  Switch} from 'react-router-dom';
 import Dashboard from './components/dashboard/Dashboard.js';
+import ProjectPage from './components/ProjectPage/ProjectPage.js';
+
+
+
+
+
 
 class App extends Component{
+
+  constructor(props)
+  {
+      super(props);
+      this.state={
+          items:[],
+      }
+    }
+
+      async componentDidMount() {
+        const items = await  (await fetch(`https://jsonplaceholder.typicode.com/users`)).json();
+        this.setState({ items});
+      }
+ 
 render()
 {
- 
-  
+
+  const {items} = this.state
   return(
   <div className = "App">
-  <Dashboard/>
-   
+ <BrowserRouter >
+
+<Route  path="/" exact component={Dashboard}/>
+<Route   path="/:item_id" render ={ props => <ProjectPage {...props} items ={items}/>
+}/>
+
+
+
+</BrowserRouter>
   
 
   </div>
